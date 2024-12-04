34a11f3c4979f1b6151e0dba1621b2a27c98c62a
you-rebel-scum <sentient21@gmail.com>
2024-12-03 18:49:52 -0800

    Initial assignment file created.

---
+++ Assignment08_JeremyPeters.py
@@ -0,0 +1,6 @@
+# --------------------------------------------------------------------------- #
+# Title: Assignment08-Starter files
+# # Description: A collection of classes for managing the application
+# ChangeLog: (Who, When, What)
+# RRoot,1.5.2030,Created Script
+# --------------------------------------------------------------------------- #
