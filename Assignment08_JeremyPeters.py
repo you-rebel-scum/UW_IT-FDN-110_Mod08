e828930bfde989fe39d6774acf9e5169b1f47e39
you-rebel-scum <39898023+you-rebel-scum@users.noreply.github.com>
2024-12-15 22:10:19 -0800

    Delete Assignment08_JeremyPeters.py

--- Assignment08_JeremyPeters.py
+++
@@ -1,6 +0,0 @@
-# --------------------------------------------------------------------------- #
-# Title: Assignment08-Starter files
-# # Description: A collection of classes for managing the application
-# ChangeLog: (Who, When, What)
-# RRoot,1.5.2030,Created Script
-# --------------------------------------------------------------------------- #
